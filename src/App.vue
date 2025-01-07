7d7010a4dc368c4ebe12bc3551695caec53c4b18
andres <andresxv@gmail.com>
2025-01-07 13:03:24 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import BeamsPanel from './components/BeamsPanel.vue'
+</script>
+
+<template>
+  <header>
+    <!-- img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" / -->
+     <h1>Vue widget for Pusher Beams</h1>
+  </header>
+
+  <main>
+    <BeamsPanel />
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/BeamsPanel.vue
@@ -0,0 +1,6 @@
+<template>
+    <button onclick="doBeamsAuth()">Start Beams Auth</button>
+    <button onclick="getBeamsUser()">Get Beams User</button>
+    <button onclick="stopBeamsAuth()">Stop Beams Auth</button>
+</template>
+
