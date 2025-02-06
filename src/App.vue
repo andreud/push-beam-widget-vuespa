392c3d79e574be449e1c042b41b57abf2ce69866
andres <andresxv@gmail.com>
2025-02-06 14:42:10 -0300

    more styles demo page

--- src/App.vue
+++ src/App.vue
@@ -23,52 +23,56 @@ const debugBeamsSubscribeInterest = ref(false);
 
   <main class="p-6 flex">
 
-    <article class="mx-6 p-7 shadow-lg rounded-lg w-1/2">
-      <h2 class="text-xl font-semibold mb-2">For specific user</h2>
+    <article class="mx-7 p-7 shadow-lg rounded-lg w-1/2">
+      <h2 class="text-xl font-semibold mb-2">👤For specific user</h2>
       <p class="text-lg mb-6 ">Let users enable personalized notifications</p>
       
-      <BeamsAuthUser
-        app-user-id="12" 
-        :beams-instance-id="instanceId"
-        :auth-endpoint="authEndpoint"
-        :debug="debugBeamsAuthUser" />
-        
-        <label for="debugBeamsAuthUser" class="my-4">
-          <input type="checkbox" v-model="debugBeamsAuthUser" id="debugBeamsAuthUser" />
-          Show Debug Info
-        </label>
+      <div class="widget-demo border-1 border-gray-600 my-4 p-4 rounded-lg shadow-sm">
+        <BeamsAuthUser
+          app-user-id="12" 
+          :beams-instance-id="instanceId"
+          :auth-endpoint="authEndpoint"
+          :debug="debugBeamsAuthUser" />
+      </div>
 
-        <p class="text-lg mb-6 ">Code</p>
+      <label for="debugBeamsAuthUser" class="">
+        <input type="checkbox" v-model="debugBeamsAuthUser" id="debugBeamsAuthUser" />
+        Show Debug Info
+      </label>
 
-        <CodeBlock>
+      <p class="text-lg pt-2">Code</p>
+
+      <CodeBlock>
   &lt;BeamsAuthUser
-    app-user-id="12" 
-    :beams-instance-id="instanceId"
-    :auth-endpoint="authEndpoint"
-    :debug="debugBeamsAuthUser" /&gt;
-        </CodeBlock>
+    beams-instance-id="Beams instance ID"
+    app-user-id="The user ID in your app" 
+    auth-endpoint="URL that must return a Beams token"
+    /&gt;
+      </CodeBlock>
       
     </article>
 
-    <article class="mx-6 p-7 shadow-lg rounded-lg w-1/2">
-      <h2 class="text-xl font-semibold mb-2 ">For interest groups</h2>
+    <article class="mx-7 p-7 shadow-lg rounded-lg w-1/2">
+      <h2 class="text-xl font-semibold mb-2 ">👥 For interest groups</h2>
       <p class="mb-6 text-lg">Let users subscribe to interests batch notifications</p>
       
-      <BeamsSubscribeInterest
-        :beams-instance-id="instanceId"
-        beams-interest-id="debug-tacos"
-        :debug="debugBeamsSubscribeInterest"
-        class="m-2" />
-      <BeamsSubscribeInterest
-        :beams-instance-id="instanceId"
-        beams-interest-id="debug-hamburgers"
-        :debug="debugBeamsSubscribeInterest"
-        class="m-2" />
-      <BeamsSubscribeInterest
-        :beams-instance-id="instanceId"
-        beams-interest-id="debug-salads"
-        :debug="debugBeamsSubscribeInterest"
-        class="m-2" />
+      <div class="widget-demo border-1 border-gray-600 p-4 rounded-lg">
+        <BeamsSubscribeInterest
+          :beams-instance-id="instanceId"
+          beams-interest-id="debug-tacos"
+          :debug="debugBeamsSubscribeInterest"
+          class="m-2" />
+        <BeamsSubscribeInterest
+          :beams-instance-id="instanceId"
+          beams-interest-id="debug-hamburgers"
+          :debug="debugBeamsSubscribeInterest"
+          class="m-2" />
+        <BeamsSubscribeInterest
+          :beams-instance-id="instanceId"
+          beams-interest-id="debug-salads"
+          :debug="debugBeamsSubscribeInterest"
+          class="m-2" />
+        </div>
 
         <label for="debugBeamsSubscribeInterest" class="mt-4">
           <input type="checkbox" v-model="debugBeamsSubscribeInterest" id="debugBeamsSubscribeInterest" />
@@ -76,17 +80,15 @@ const debugBeamsSubscribeInterest = ref(false);
         </label>
 
         <CodeBlock>
-      &lt;BeamsSubscribeInterest
-        :beams-instance-id="instanceId"
-        beams-interest-id="debug-tacos"
-        :debug="bool"
-      /&gt;
+    &lt;BeamsSubscribeInterest
+      beams-instance-id="Beams instance ID"
+      beams-interest-id="The ID of the interest to subscribe"
+    /&gt;
 
-      &lt;BeamsSubscribeInterest
-        :beams-instance-id="instanceId"
-        beams-interest-id="debug-hamburgers"
-        :debug="bool"
-      /&gt;
+    &lt;BeamsSubscribeInterest
+      beams-instance-id="Beams instance ID"
+      beams-interest-id="The ID of the interest to subscribe"
+    /&gt;
         </CodeBlock>
       
     </article>
